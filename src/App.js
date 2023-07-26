0e18183d245716db77d53fe57a8db48305d1fa9a
Oleh <kravetsoleh4991@gmail.com>
2023-07-25 18:16:32 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const GrandChildOne = (props) => {
+  return (
+    <>
+      <h3>{props.bookTitle}</h3>
+      <h3>{props.author}</h3>
+    </>
+  )
+}
+
+
+const ChildOne = (props) => {
+  return (
+    <>
+      <h1>{props.bookTitle}</h1>
+      {props.author && <GrandChildOne bookTitle={props.bookTitle} author={props.author}/>}
+    </>
+  )
+}
+
+const Books = (props) => {
+  return (
+    <>
+      <h1>{props.bookTitle}</h1>
+      {props.author && <BooksWithAuthor bookTitle={props.bookTitle} author={props.author}/>}
+    {/* either or work */}
+      {/* {props.author ? <BooksWithAuthor bookTitle={props.bookTitle} author={props.author}/> : null} */}
+    </>
+  )
+}
+
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChildOne bookTitle='Goosebumps' />
+      <ChildOne bookTitle='Twilight' />
+      <ChildOne author='jkrawling' bookTitle='Harry Potter' />
     </div>
   );
 }
 
 export default App;
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// const GrandChildOne = (props) => {
+//   return (
+//     <>
+//       <h1>{props.bookTitle}</h1>
+//       <h1>{props.author}</h1>
+//     </>
+//   )
+// }
+
+
+// const ChildOne = (props) => {
+//   return (
+//     <h1>{props.bookTitle}</h1>
+//   )
+// }
+
+// const ChildOneVersionTwo = (props) => {
+//   return (
+//     <>
+//       <h1>{props.bookTitle}</h1>
+//       <GrandChildOne
+//         bookTitle={props.bookTitle}
+//         author={props.author}
+//       />
+//     </>
+//   )
+// }
+
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <ChildOne bookTitle='Goosebumps' />
+//       <ChildOne bookTitle='Twilight' />
+//       <ChildOneVersionTwo author='jkrawling' bookTitle='Harry Potter' />
+//     </div>
+//   );
+// }
+
+// export default App;
+
